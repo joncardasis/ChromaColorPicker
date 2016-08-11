5c04c2cb8f9f8cee056a61fc3a1dd32b51dc5011
Cardasis , Jonathan (J.) <jcardasi@mgc000001684.eccrf.ford.com>
2016-08-11 14:02:26 -0400

    Initial Commit

---
+++ JCColorPicker-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JCColorPicker-Demo
+//
+//  Created by Cardasis, Jonathan (J.) on 8/11/16.
+//  Copyright © 2016 Jonathan Cardasis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
