3a8ab880d3b394fece8169ce971c5442d68f535d
Jonathan Cardasis <joncardasis@gmail.com>
2018-01-07 17:23:18 -0500

    Fixed UIColor hexcode representation for edge cases (greys)

--- ChromaColorPicker/UIColor+Utilities.swift
+++ ChromaColorPicker/UIColor+Utilities.swift
@@ -26,8 +26,11 @@ import UIKit
 public extension UIColor{
     public var hexCode: String {
         get{
-            let colorComponents = self.cgColor.components
-            return String(format: "%02x%02x%02x", Int(colorComponents![0]*255.0), Int(colorComponents![1]*255.0),Int(colorComponents![2]*255.0)).uppercased()
+            let colorComponents = self.cgColor.components!
+            if colorComponents.count < 4 {
+                return String(format: "%02x%02x%02x", Int(colorComponents[0]*255.0), Int(colorComponents[0]*255.0),Int(colorComponents[0]*255.0)).uppercased()
+            }
+            return String(format: "%02x%02x%02x", Int(colorComponents[0]*255.0), Int(colorComponents[1]*255.0),Int(colorComponents[2]*255.0)).uppercased()
         }
     }
     
