f7ae25ef4a9a7c2217d0d25e1e182612208aaebe
Jonathan Cardasis <joncardasis@gmail.com>
2019-04-16 21:44:48 -0400

    Updated umbrella header

--- Source/ChromaColorPicker.h
+++ Source/ChromaColorPicker.h
@@ -6,14 +6,7 @@
 //  Copyright © 2017 Jonathan Cardasis. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
-//! Project version number for ChromaColorPicker.
 FOUNDATION_EXPORT double ChromaColorPickerVersionNumber;
-
-//! Project version string for ChromaColorPicker.
 FOUNDATION_EXPORT const unsigned char ChromaColorPickerVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ChromaColorPicker/PublicHeader.h>
-
-
